adbe8b25a15cf73ad037b2e95845662954082690
dan <dan.chen.auckland@outlook.com>
2019-09-09 03:55:28 +0000

    finish install

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://k8s.digital/admin/');
+define('HTTP_CATALOG', 'http://k8s.digital/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://k8s.digital/admin/');
+define('HTTPS_CATALOG', 'http://k8s.digital/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/opencart/upload/admin/');
+define('DIR_SYSTEM', '/var/www/html/opencart/upload/system/');
+define('DIR_IMAGE', '/var/www/html/opencart/upload/image/');
+define('DIR_STORAGE', '/var/www/html/opencart/storage/');
+define('DIR_CATALOG', '/var/www/html/opencart/upload/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '192.168.1.9');
+define('DB_USERNAME', 'dan');
+define('DB_PASSWORD', 'Ird2019@');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ system/storage/cache/dc/dcaf406e7da4c28feac2cc0f891d309b545f97a36dc05837a8697a28617c4137.php
@@ -0,0 +1,52 @@
+<?php
+
+/* default/template/common/search.twig */
+class __TwigTemplate_c13709c54b7876634e35ca904c91956adf62bb9a91475a9dda053688f8d3b684 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<div id=\"search\" class=\"input-group\">
+  <input type=\"text\" name=\"search\" value=\"";
+        // line 2
+        echo (isset($context["search"]) ? $context["search"] : null);
+        echo "\" placeholder=\"";
+        echo (isset($context["text_search"]) ? $context["text_search"] : null);
+        echo "\" class=\"form-control input-lg\" />
+  <span class=\"input-group-btn\">
+    <button type=\"button\" class=\"btn btn-default btn-lg\"><i class=\"fa fa-search\"></i></button>
+  </span>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "default/template/common/search.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  22 => 2,  19 => 1,);
+    }
+}
+/* <div id="search" class="input-group">*/
+/*   <input type="text" name="search" value="{{ search }}" placeholder="{{ text_search }}" class="form-control input-lg" />*/
+/*   <span class="input-group-btn">*/
+/*     <button type="button" class="btn btn-default btn-lg"><i class="fa fa-search"></i></button>*/
+/*   </span>*/
+/* </div>*/
